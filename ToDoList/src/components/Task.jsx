94d7a1ff3dbc390674e4b377209bb13e42aad5a8
Eduardo Alves <eduardo2005alve@gmail.com>
2025-06-14 09:44:22 -0300

    Update Task.jsx

--- ToDoList/src/components/Task.jsx
+++ ToDoList/src/components/Task.jsx
@@ -17,12 +17,5 @@ function Task({ task, index, removeTask, toggleComplete }) {
   export default Task;
   
 
-  /*
-  Esse componente é a tarefa que aparece na lista.
-  Ele recebe a tarefa (task), o índice dela (index), e duas funções: removeTask e toggleComplete.
-  Dentro dele, temos:
-  Um checkbox (caixinha de marcar) que, se a tarefa estiver marcada como "completa", ela fica marcada.
-  Quando clica nele, ele chama a função toggleComplete e passa o índice da tarefa pra alternar se ela está concluída ou não.
-  Um <span> que mostra o texto da tarefa. Se a tarefa estiver completada, o texto fica riscado.
-  Um botão "Remover" que, quando clicado, chama a função removeTask com o índice da tarefa pra remover ela da lista.
-   */
+
+
