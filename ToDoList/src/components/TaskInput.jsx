41d4d9c95d5f8b79f7f498e32d6f1fb0e4419a7b
Eduardo Alves <eduardo2005alve@gmail.com>
2025-06-14 09:44:35 -0300

    Update TaskInput.jsx

--- ToDoList/src/components/TaskInput.jsx
+++ ToDoList/src/components/TaskInput.jsx
@@ -27,14 +27,4 @@ function TaskInput({ addTask }) {
 export default TaskInput;
 
 
-/*
-Esse é o componente onde a gente digita e adiciona uma nova tarefa.
-Ele usa o hook useState pra guardar o que é digitado no input (campo de texto).
-função handleAddTask é chamada quando clica no botão "Adicionar":
-Se a tarefa não estiver vazia (a gente usa trim() pra garantir que não seja só espaço), ele chama a função addTask passando o texto da tarefa.
-Depois limpa o campo de texto (setTask('')).
-Dentro do JSX, temos:
-Um campo de texto (<input>) onde o usuário digita a tarefa. A cada letra digitada, o valor de "task" é atualizado.
-Um botão "Adicionar" que, quando clicado, chama a função handleAddTask pra adicionar a tarefa.
-
 */
