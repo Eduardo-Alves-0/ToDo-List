9bc2dc1339876c40de404090b58f9ccdd3a54963
Eduardo Alves <eduardo2005alve@gmail.com>
2025-06-14 09:44:48 -0300

    Update TaskList.jsx

--- ToDoList/src/components/TaskList.jsx
+++ ToDoList/src/components/TaskList.jsx
@@ -17,16 +17,3 @@ function TaskList({ tasks, removeTask, toggleComplete }) {
 }
 
 export default TaskList;
-
-
-/*
-  Esse componente é onde a lista de tarefas é exibida.
-  Ele recebe a lista de tarefas (tasks), a função para remover a tarefa (removeTask) e a função para alternar o estado da tarefa (toggleComplete).
-  O que ele faz:
-  Cria uma lista de itens (<ul>).
-  Para cada tarefa em "tasks", ele cria um componente <Task> (que é o componente que mostra cada tarefa individualmente).
-  Cada <Task> recebe:
-  A chave (key) com o índice da tarefa (para o React identificar cada item de forma única).
-  O índice, a tarefa, e as funções de remover e alternar o estado da tarefa.
-  Isso vai gerar uma lista com todos os componentes de tarefas que o usuário tem.
-*/
